b087a1df5f971f3925117c7376141473abed2d84
서상현 <sibjagun99@naver.com>
2024-03-27 17:38:41 +0900

    Clean: "테스트용 파일들 제외하고 필요없는 패키지, 폴더 삭제"

--- ANIS/src/main/java/com/npt/anis/ANIS/repository/TestMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.npt.anis.ANIS.repository;
-
-import com.npt.anis.ANIS.domain.TestMongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestMongoRepository extends MongoRepository<TestMongo, String> {
-}
--- ANIS/src/main/java/com/npt/anis/ANIS/repository/TestUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.npt.anis.ANIS.repository;
-
-import com.npt.anis.ANIS.domain.TestUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestUserRepository extends JpaRepository<TestUser,Long> {
-}
