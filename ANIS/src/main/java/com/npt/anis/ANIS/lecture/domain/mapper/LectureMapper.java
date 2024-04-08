5e4b0a13f43f73f21501445abb77a61c6af5ade9
서상현 <sibjagun99@naver.com>
2024-04-08 16:15:42 +0900

    Add: "Mapper 생성"

---
+++ ANIS/src/main/java/com/npt/anis/ANIS/lecture/domain/mapper/LectureMapper.java
@@ -0,0 +1,27 @@
+package com.npt.anis.ANIS.lecture.domain.mapper;
+
+import com.npt.anis.ANIS.lecture.domain.dto.LectureDto;
+import com.npt.anis.ANIS.lecture.domain.entity.Lecture;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface LectureMapper {
+    LectureDto toDto(Lecture lecture);
+
+    Lecture toEntity(LectureDto lectureDto);
+
+    @Mapping(target = "lecName", source = "lecName", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "lecCredit", source = "lecCredit", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "lecYear", source = "lecYear", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "lecSemester", source = "lecSemester", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "lecProfessor", source = "lecProfessor", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "lecDay", source = "lecDay", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "lecTimeStart", source = "lecTimeStart", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "lecTimeEnd", source = "lecTimeEnd", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(LectureDto dto, @MappingTarget Lecture entity);
+}
---
+++ ANIS/src/main/java/com/npt/anis/ANIS/lecture/domain/mapper/LecturePresetMapper.java
@@ -0,0 +1,8 @@
+package com.npt.anis.ANIS.lecture.domain.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface LecturePresetMapper {
+
+}
