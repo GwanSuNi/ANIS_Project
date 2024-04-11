aecc204f34f02e950d7716d8c6a27cd916ecf046
윤우 <tecte1023@gmail.com>
2024-04-11 17:38:22 +0900

    Feat: Assessment CRUD API 구현
    - Assessment Entity 추가
    - Assessment DTO 추가
    - Assessment Mapper 추가
    - Assessment Repository 추가
    - Assessment Service 추가
    - Assessment Controller 추가

---
+++ ANIS/src/main/java/com/npt/anis/ANIS/assessment/service/AssessmentService.java
@@ -0,0 +1,7 @@
+package com.npt.anis.ANIS.assessment.service;
+
+import com.npt.anis.ANIS.assessment.domain.dto.AssessmentDto;
+import com.npt.anis.ANIS.global.service.CrudService;
+
+public interface AssessmentService extends CrudService<AssessmentDto, Long> {
+}
