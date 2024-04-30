cab783df56c04ad80f8f28c20b0ff90104af1221
관형 <tony2yoo@gmail.com>
2024-04-30 14:55:21 +0900

    Chore: 리액트 CRA 템플릿 타입스크립트로 변경
    변경하는 과정에서 리액트 프록시 설정에 관해서 문제가 있었는데,
    백엔드 서버에 api 요청 시 404 not found가 뜨는 이슈가 있었다...
    하루종일 삽질하다 발견한 블로그
    https://egas.tistory.com/39
    
    그냥 TS로는 PR을 안올려서 지원이 안되는거였다니...

---
+++ ANIS/src/main/anis_front/src/App.tsx
@@ -0,0 +1,41 @@
+import './App.css';
+
+import { useEffect, useState } from "react";
+import axios, {AxiosResponse} from "axios";
+
+function App() {
+  const [hello, setHello] = useState<string>('');
+
+
+// 토큰을 가져옵니다. 이 예제에서는 localStorage에서 토큰을 가져옵니다.
+    const token = localStorage.getItem('access');
+
+// Axios 인스턴스를 생성합니다.
+    const instance = axios.create({
+        baseURL: '/api',
+        timeout: 1000,
+        headers: {'Authorization': `Bearer ${token}`}
+    });
+
+    // 이제 이 인스턴스를 사용하여 요청을 보낼 수 있습니다.
+    instance.get('/test')
+        .then(response => {
+            console.log(response.data);
+        });
+
+  // useEffect(() => {
+  //   axios.get('/api/test')
+  //       .then((res: AxiosResponse<string>) => {
+  //         setHello(res.data);
+  //       });
+  // }, []);
+
+  return (
+      <div className="App">
+          {token}
+        {/*백엔드 데이터 : {hello}*/}
+      </div>
+  );
+}
+
+export default App;
