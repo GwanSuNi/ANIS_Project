d45a6150bd18c08ac55978f9720b658276159d90
윤우 <tecte1023@gmail.com>
2024-05-13 15:37:12 +0900

    Add: TODO 추가

--- ANIS/src/main/anis_front/src/pages/SurveyListPage.tsx
+++ ANIS/src/main/anis_front/src/pages/SurveyListPage.tsx
@@ -6,6 +6,11 @@ import CircularProgressWithLabel from '../components/common/CircularProgressWith
 import {SurveySection} from '../components/survey';
 
 export default function SurveyListPage() {
+    //TODO: 서버로부터 데이터 받아와야 함
+    const assessmentCount = 10; // 총 진단평가 개수
+    const completeCount = 4; // 완료된 진단평가 개수
+    const incompleteCount = 6; // 미완료된 진단평가 개수
+
     return (
         <Container maxWidth='lg'>
             <Box display='flex' justifyContent='space-between' mb={8}>
@@ -16,11 +21,11 @@ export default function SurveyListPage() {
                     <Typography variant='h4'>
                         진행률
                     </Typography>
-                    <CircularProgressWithLabel value={100}/>
+                    <CircularProgressWithLabel value={completeCount / assessmentCount * 100}/>
                 </Stack>
             </Box>
-            <SurveySection isComplete={false} count={5}/>
-            <SurveySection isComplete={true} count={5}/>
+            <SurveySection isComplete={false} count={incompleteCount}/>
+            <SurveySection isComplete={true} count={completeCount}/>
         </Container>
     );
 }
