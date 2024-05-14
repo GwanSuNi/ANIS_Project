5cc98e327c6521fd73b4e0372a108c0f3ae5bb7e
관형 <tony2yoo@gmail.com>
2024-05-14 17:33:23 +0900

    Add: 초기 데이터 실행 등 공통 sql 파일
    TODO: 스프링 run시 sql 실행하게 설정

---
+++ ANIS/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Department (DEP_INDEX, DEP_NAME) VALUES (1, '라이프서비스과');
+INSERT INTO Department (DEP_INDEX, DEP_NAME) VALUES (2, '라이프케어과');
+INSERT INTO Department (DEP_INDEX, DEP_NAME) VALUES (3, '라이프콘텐츠과');
+INSERT INTO Department (DEP_INDEX, DEP_NAME) VALUES (4, '라이프테크과');
