dfd84172ad7108ed11204e4dfdbc945ced42e5e0
서상현 <sibjagun99@naver.com>
2024-05-16 02:22:01 +0900

    Add: "수강신청 기능 완료"

--- ANIS/src/main/java/com/npt/anis/ANIS/lecture/repository/LectureRegisteredRepository.java
+++ ANIS/src/main/java/com/npt/anis/ANIS/lecture/repository/LectureRegisteredRepository.java
@@ -9,13 +9,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface LectureRegisteredRepository extends JpaRepository<LectureRegistered,Long> {
-
-    @Query("SELECT l as lecture , lr as lectureRegistered " +
-            "from Lecture l " +
-            "left join LectureRegistered lr " +
-            "on lr.lecID = l.lecID " +
-            "WHERE lr.studentID = :studentID ")
+    @Query("SELECT l as lecture, lr.selected as selected, lr.lecYear as lecYear, lr.lecSemester as lecSemester " +
+        "FROM Lecture l " +
+        "LEFT JOIN LectureRegistered lr " +
+        "ON lr.lecID = l.lecID " +
+        "WHERE lr.studentID = :studentID")
     List<Tuple> getByLectureRegisteredWithLecture(@Param("studentID") String studentID);
+
 
     List<LectureRegistered> findByStudentID(String studentID);
 
