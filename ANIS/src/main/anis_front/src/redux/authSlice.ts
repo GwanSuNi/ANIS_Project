ed8d27b693db267c0c1b45268b1e19a39bd2c690
윤우 <tecte1023@gmail.com>
2024-05-20 15:31:20 +0900

    Comment: 주석 추가

--- ANIS/src/main/anis_front/src/redux/authSlice.ts
+++ ANIS/src/main/anis_front/src/redux/authSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// utils/authSlice.ts의 isLoggedIn() 함수로 대처
+// 페이지 새로고침하면 리덕스 값이 초기화가 되서 로그인 상태가 유지가 안됨
 const authSlice = createSlice({
     name: 'authSlice',
     initialState: { isLoggedIn: false },
