e21ef93edd9a6052a8ff86c0b062a5ef85ba0fab
관형 <tony2yoo@gmail.com>
2024-05-30 19:18:11 +0900

    Feat: React Query를 적용시킨 MyInfo
    - 디렉터리도 변경하였음

--- ANIS/src/main/anis_front/src/components/myInfo/MyInfo.tsx
+++ ANIS/src/main/anis_front/src/components/myInfo/MyInfo.tsx
@@ -1,19 +1,18 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import * as React from "react";
 import Container from "@mui/material/Container";
-import {Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Box, Skeleton, Table, TableBody, TableCell, TableContainer, TableRow} from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Typography from '@mui/material/Typography';
-import {useEffect, useState} from "react";
-import secInstance from "../utils/secInstance";
 import Paper from "@mui/material/Paper";
 import Card from "@mui/material/Card";
 import CardMedia from '@mui/material/CardMedia';
-import {useUserInfo} from "../hooks/useUserInfo";
-import useProfileImage from "../hooks/useProfileImage";
-import useQrCode from "../hooks/useQrCode";
+import {useUserInfo} from "../../hooks/useUserInfo";
+import useQrCode from "../../hooks/useQrCode";
+import useUpdateProfileImage from "../../hooks/useUpdateProfileImage";
+import ImageCard from "./ImageCard";
 
-interface UserInfo {
+export interface UserInfo {
     studentID: string;
     studentName: string;
     departmentName: string;
@@ -29,14 +28,15 @@ function MyInfoTable(userInfo: UserInfo) {
         {label: '생년월일', value: birth},
     ];
     return (
-        <TableContainer sx={{minWidth: '250px', minHeight:'250px', maxWidth:'300px', maxHeight:'300px'}} component={Paper}>
-            <Table  size="medium" aria-label="a dense table">
+        <TableContainer sx={{minWidth: '250px', minHeight: '250px', maxWidth: '300px', maxHeight: '300px'}}
+                        component={Paper}>
+            <Table size="medium" aria-label="a dense table">
                 <TableBody>
                     {data.map((row) => (
                         <TableRow key={row.label}>
                             <TableCell component="th" scope="row">
                                 <Typography variant='h6'>
-                                {row.label}
+                                    {row.label}
                                 </Typography>
                             </TableCell>
                             <TableCell align="right"><Typography variant='h6'>{row.value}</Typography></TableCell>
@@ -51,7 +51,29 @@ function MyInfoTable(userInfo: UserInfo) {
 export default function MyInfo() {
     const userInfo = useUserInfo();
     const qrCode = useQrCode();
-    const profileImage = useProfileImage(userInfo?.studentID);
+    const {mutate: updateProfileImage} = useUpdateProfileImage();
+
+    const handleUploadClick = async (studentID:string) => {
+        // 파일 선택 요소를 동적으로 생성합니다.
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = 'image/*'; // 이미지만 선택할 수 있도록 합니다.
+
+        // 파일 선택 요청을 처리합니다.
+        fileInput.addEventListener('change', (event) => {
+            /// event.target이 null인지 확인합니다.
+            if (fileInput.files === null) {
+                return;
+            }
+            const file = fileInput.files[0];
+            if (file) {
+                updateProfileImage({ studentID, file });
+            }
+        });
+
+        // 파일 선택 요소를 클릭하여 파일 선택 대화상자를 엽니다.
+        fileInput.click();
+    };
 
     if (!userInfo) {
         return <div>로딩중 입니다...</div>;
@@ -68,30 +90,32 @@ export default function MyInfo() {
                     width: '100%',
                 }}
             >
-                <Grid container spacing={2} maxWidth={"xl"} direction={"column"} justifyContent="center" alignItems="center" >
-                    <Grid container spacing={4} xs={12} justifyContent="center" alignItems="center"  direction={{ xs: 'column', sm: 'row' }}
+                <Grid container spacing={2} maxWidth={"xl"} direction={"column"} justifyContent="center"
+                      alignItems="center">
+                    <Grid container spacing={4} xs={12} justifyContent="center" alignItems="center"
+                          direction={{xs: 'column', sm: 'row'}}
                           width={'900px'}
-                          columnSpacing={{ xs: 2, sm: 4, md: 4 }}
-                          rowSpacing={{ xs: 2, sm: 4, md: 4 }}
-                          sx={{p:0, mt:3}}>
+                          columnSpacing={{xs: 2, sm: 4, md: 4}}
+                          rowSpacing={{xs: 2, sm: 4, md: 4}}
+                          sx={{p: 0, mt: 3}}>
                         <Grid xs={'auto'} display={'flex'} justifyContent="center" alignItems="center">
                             {/*    이미지 박스*/}
                             {/*    TODO: DB에서 본인 증명사진 받아오기 */}
-                            <Card
-                                style={{width:'203px', height:'260px'}}>
-                                <CardMedia
-                                    component="img"
-                                    image={profileImage}
-                                    alt="profile"
-                                />
-                            </Card>
+                            <ImageCard userInfo={userInfo} handleUploadClick={handleUploadClick}/>
+                            {/*{Image(handleUploadClick, userInfo.studentID)}*/}
                         </Grid>
                         <Grid container xs={6} justifyContent="center" alignItems="center">
                             <MyInfoTable {...userInfo}/>
                         </Grid>
                     </Grid>
                     <Grid display={'flex'} xs={12} justifyContent="center" alignItems="center">
-                        <Card style={{minWidth: '100px', minHeight: '100px', maxWidth:'300px', maxHeight:'300px', marginTop:10}}>
+                        <Card style={{
+                            minWidth: '100px',
+                            minHeight: '100px',
+                            maxWidth: '300px',
+                            maxHeight: '300px',
+                            marginTop: 10
+                        }}>
                             <CardMedia
                                 component="img"
                                 src={qrCode}
@@ -104,5 +128,6 @@ export default function MyInfo() {
         </Container>
 
 
-    );
+    )
+        ;
 };
