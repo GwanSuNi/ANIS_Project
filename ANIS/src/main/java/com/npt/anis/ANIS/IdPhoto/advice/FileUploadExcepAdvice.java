9042a61b7f753ede2e31566a8322ed4a8d476aab
관형 <tony2yoo@gmail.com>
2024-05-30 19:16:06 +0900

    Feat: 파일 업로드 크기 제한 조정 및 파일크기 ExceptionHandler 추가
    - 일단 웬만하면 문제 생기지 않게 5MB로 넉넉하게 주었으나, 데이터 스토리지 용량을 감안해 줄여야할 수도 있음

---
+++ ANIS/src/main/java/com/npt/anis/ANIS/IdPhoto/advice/FileUploadExcepAdvice.java
@@ -0,0 +1,15 @@
+package com.npt.anis.ANIS.IdPhoto.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+public class FileUploadExcepAdvice {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("파일의 크기가 너무 큽니다!");
+    }
+}
