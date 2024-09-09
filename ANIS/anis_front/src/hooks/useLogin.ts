6ada45f342f9ad2625c36eef9ecd1d4916a78481
관형 <tony2yoo@gmail.com>
2024-09-09 14:25:08 +0900

    Improve: 로그인 로직에 FCM Token 관련 기능 추가
    - 로그인 하기 전 : FCM Token 가져오기
    - 로그인 할 때 : 아이디 비밀번호와 함께 FCM Token을 서버로 전송하기

--- ANIS/anis_front/src/hooks/useLogin.ts
+++ ANIS/anis_front/src/hooks/useLogin.ts
@@ -1,13 +1,23 @@
-import {FormEvent, useState} from 'react';
+import {FormEvent, useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 import {loginInstance} from '@utils';
 import {useNavigate} from 'react-router-dom';
 import {RootState} from '@redux';
+import {useFirebase} from "./useFirebase";
 
 export function useLogin() {
     const username = useSelector((state: RootState) => state.username.username);
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const firebaseToken = useFirebase();
+    const [token, setToken] = useState<string | null>(null);
+
+    // Firebase 토큰이 준비되면 상태 업데이트
+    useEffect(() => {
+        if (firebaseToken) {
+            setToken(firebaseToken);
+        }
+    }, [firebaseToken]);
 
     const handleSubmit = async (event?: FormEvent) => {
         if (event) {
@@ -16,6 +26,9 @@ export function useLogin() {
         const formData = new FormData();
         formData.append('username', username);
         formData.append('password', password);
+        if (token) {
+            formData.append('fcmToken', token);
+        }
         try {
             await loginInstance.post('/api/login', formData, {
                 headers: {
